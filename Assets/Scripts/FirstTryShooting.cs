69e23ed96d47efa75a0a39bf9c48e174effed2f8
mlindfors2 <31898439+mlindfors2@users.noreply.github.com>
2019-05-08 15:58:47 +0200

    Added Temporary objects for shooting
    Trying to figure out how to hit something

---
+++ Assets/Scripts/FirstTryShooting.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstTryShooting : MonoBehaviour {
+
+    public GameObject bullet;
+    public float bulletSpeed = 100f;
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            GameObject instBullet = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
+            Rigidbody instBulletRigidBody = instBullet.GetComponent<Rigidbody>();
+            instBulletRigidBody.AddForce(target.transform.position * bulletSpeed);
+            //instBulletRigidBody.AddForce(Vector3.forward * bulletSpeed);
+
+            Destroy(instBullet, 3.0f);
+
+        }
+    }
+}
