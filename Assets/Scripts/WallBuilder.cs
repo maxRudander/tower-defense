e3fc068fc0e3f9e1e39ee8321ef166cff113f70c
mlindfors2 <31898439+mlindfors2@users.noreply.github.com>
2019-05-08 13:26:25 +0200

    Added brickwall
    Added brickwall as alternative walls around map

--- Assets/Scripts/WallBuilder.cs
+++ Assets/Scripts/WallBuilder.cs
@@ -6,11 +6,15 @@ public class WallBuilder : MonoBehaviour {
     public Transform pointPreFabTree; //första trädet
     [Range(1, 100)]
     public int numberOfTrees = 10;
-    [Range(1,100)]
+    [Range(1, 100)]
     public int distanceBetweenTrees = 30;
+    [Range(1, 10)]
+    public int numberOfRows = 1;
+
     private Transform[] points;
     public bool Horizontal = true;
-    public bool Vertical = false; 
+    public bool Vertical = false;
+
 
 
     // Start is called before the first frame update
@@ -34,23 +38,32 @@ public class WallBuilder : MonoBehaviour {
         int zincrease = 0;
         if (Horizontal && !Vertical) {
             xincrease = distanceBetweenTrees;
-        }
-        else if (Vertical && !Horizontal) {
+        } else if (Vertical && !Horizontal) {
             zincrease = distanceBetweenTrees;
         }
-        
-        int x = distanceBetweenTrees/2;
-        int z = distanceBetweenTrees/2;
-        for (int i = 0; i < points.Length; i++) {
-            Transform point = Instantiate(pointPreFabTree);
-            point.SetParent(transform, false);
-            Vector3 p = new Vector3(x , 0, z );
-            point.localPosition += p;
-            point.localRotation = Quaternion.Euler(-90f, (float)-Random.Range(0, 360), 0f);
-            points[i] = point;
-            x = x + xincrease;
-            z = z + zincrease;
 
+        int x = distanceBetweenTrees / 2;
+        int z = distanceBetweenTrees / 2;
+        for (int row = 0; row < numberOfRows; row++) {
+            for (int j = 0; j < points.Length; j++) {
+                Transform point = Instantiate(pointPreFabTree);
+                point.SetParent(transform, false);
+                Vector3 p = new Vector3(x, 0, z);
+                point.localPosition += p;
+                point.localRotation = Quaternion.Euler(-90f, (float)-Random.Range(0, 360), 0f);
+                points[j] = point;
+                x = x + xincrease;
+                z = z + zincrease;
+
+            }
+            if (Horizontal) {
+                z = z - distanceBetweenTrees;
+                x = distanceBetweenTrees / 2;
+            }
+            else {
+                z = distanceBetweenTrees / 2;
+                x = x + distanceBetweenTrees;
+            }
         }
     }
 }
