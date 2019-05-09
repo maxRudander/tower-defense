73102f0251122cc3ddfa13b740bddb2f6e4b6dab
mlindfors2 <31898439+mlindfors2@users.noreply.github.com>
2019-05-09 12:23:42 +0200

    Small changes to shooting

--- Assets/Scripts/FirstTryShooting.cs
+++ Assets/Scripts/FirstTryShooting.cs
@@ -18,11 +18,35 @@ public class FirstTryShooting : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.Space)) {
             GameObject instBullet = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
             Rigidbody instBulletRigidBody = instBullet.GetComponent<Rigidbody>();
-            instBulletRigidBody.AddForce(target.transform.position * bulletSpeed);
-            //instBulletRigidBody.AddForce(Vector3.forward * bulletSpeed);
+            //instBulletRigidBody.AddForce(target.transform.position * bulletSpeed);
+            ////instBulletRigidBody.AddForce(Vector3.forward * bulletSpeed);
 
-            Destroy(instBullet, 3.0f);
+            //Destroy(instBullet, 3.0f);
 
+
+
+
+            Vector3 dir = target.transform.position - instBulletRigidBody.transform.localPosition;
+
+            float distThisFrame = bulletSpeed * Time.deltaTime;
+
+            if (dir.magnitude <= distThisFrame) {
+                // We reached the node
+                Debug.Log("HIT!");
+            } else {
+                // TODO: Consider ways to smooth this motion.
+
+                // Move towards node
+                target.transform.Translate(dir.normalized * distThisFrame, Space.World);
+                Quaternion targetRotation = Quaternion.LookRotation(dir);
+                instBulletRigidBody.transform.rotation = Quaternion.Lerp(instBulletRigidBody.transform.rotation, targetRotation, Time.deltaTime * 5);
+
+
+
+
+
+
+            }
         }
     }
 }
