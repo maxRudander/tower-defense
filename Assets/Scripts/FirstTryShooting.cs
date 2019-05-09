d92fdc1bb04edb658c644de5e7cca635bc68d054
mlindfors2 <31898439+mlindfors2@users.noreply.github.com>
2019-05-09 16:44:25 +0200

    test123

---
+++ Assets/Scripts/FirstTryShooting.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirstTryShooting : MonoBehaviour {
+
+    public GameObject bullet;
+    public float bulletSpeed = 100f;
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            GameObject instBullet = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
+            Rigidbody instBulletRigidBody = instBullet.GetComponent<Rigidbody>();
+<<<<<<< HEAD
+            instBulletRigidBody.AddForce(target.transform.position * bulletSpeed);
+            instBulletRigidBody.AddForce(Vector3.forward * bulletSpeed);
+=======
+            instBulletRigidBody.AddForce((target.transform.position - this.transform.position).normalized * bulletSpeed);
+            //instBulletRigidBody.AddForce(Vector3.forward * bulletSpeed);
+>>>>>>> parent of 81bb72e... 6fgddgdg
+
+            //Destroy(instBullet, 3.0f);
+
+
+
+
+            //    Vector3 dir = target.transform.position - instBulletRigidBody.transform.localPosition;
+
+            //float distThisFrame = bulletSpeed * Time.deltaTime;
+
+            //if (dir.magnitude <= distThisFrame) {
+            //    // We reached the node
+            //    Debug.Log("HIT!");
+            //} else {
+            //    // TODO: Consider ways to smooth this motion.
+
+            //    //Move towards node
+            //    target.transform.Translate(dir.normalized * distThisFrame, Space.World);
+            //    Quaternion targetRotation = Quaternion.LookRotation(dir);
+            //    instBulletRigidBody.transform.rotation = Quaternion.Lerp(instBulletRigidBody.transform.rotation, targetRotation, Time.deltaTime * 5);
+
+
+
+
+
+
+        }
+    }
+    }
+<<<<<<< HEAD
+}
+=======
+
+>>>>>>> parent of 81bb72e... 6fgddgdg
---
+++ Assets/Scripts/WallBuilder.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallBuilder : MonoBehaviour {
+    public Transform pointPreFabTree; //första trädet
+    [Range(1, 100)]
+    public int numberOfTrees = 10;
+    [Range(1, 100)]
+    public int distanceBetweenTrees = 30;
+    [Range(1, 10)]
+    public int numberOfRows = 1;
+
+    private Transform[] points;
+    public bool Horizontal = true;
+    public bool Vertical = false;
+
+
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+    void Initilize() {
+
+    }
+    private void Awake() {
+        points = new Transform[numberOfTrees];
+        BuildWall();
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+    void BuildWall() {
+        int xincrease = 0;
+        int zincrease = 0;
+        if (Horizontal && !Vertical) {
+            xincrease = distanceBetweenTrees;
+        } else if (Vertical && !Horizontal) {
+            zincrease = distanceBetweenTrees;
+        }
+
+        int x = distanceBetweenTrees / 2;
+        int z = distanceBetweenTrees / 2;
+        for (int row = 0; row < numberOfRows; row++) {
+            for (int j = 0; j < points.Length; j++) {
+                Transform point = Instantiate(pointPreFabTree);
+                point.SetParent(transform, false);
+                Vector3 p = new Vector3(x, 0, z);
+                point.localPosition += p;
+                point.localRotation = Quaternion.Euler(-90f, (float)-Random.Range(0, 360), 0f);
+                points[j] = point;
+                x = x + xincrease;
+                z = z + zincrease;
+
+            }
+            if (Horizontal) {
+                z = z - distanceBetweenTrees;
+                x = distanceBetweenTrees / 2;
+            }
+            else {
+                z = distanceBetweenTrees / 2;
+                x = x + distanceBetweenTrees;
+            }
+        }
+    }
+}
