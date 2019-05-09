e55692c66185a260321786bb3cd62c1a1bf4c9da
mlindfors2 <31898439+mlindfors2@users.noreply.github.com>
2019-05-09 20:26:45 +0200

    script fil corrupt efter merge
    justerat script som blev corrupt av någon konstig anledning

--- Assets/Scripts/FirstTryShooting.cs
+++ Assets/Scripts/FirstTryShooting.cs
@@ -18,44 +18,15 @@ public class FirstTryShooting : MonoBehaviour {
         if (Input.GetKeyDown(KeyCode.Space)) {
             GameObject instBullet = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
             Rigidbody instBulletRigidBody = instBullet.GetComponent<Rigidbody>();
-<<<<<<< HEAD
-            instBulletRigidBody.AddForce(target.transform.position * bulletSpeed);
-            instBulletRigidBody.AddForce(Vector3.forward * bulletSpeed);
-=======
+
+            //instBulletRigidBody.AddForce(target.transform.position * bulletSpeed);
+            //instBulletRigidBody.AddForce(Vector3.forward * bulletSpeed);
+
             instBulletRigidBody.AddForce((target.transform.position - this.transform.position).normalized * bulletSpeed);
             //instBulletRigidBody.AddForce(Vector3.forward * bulletSpeed);
->>>>>>> parent of 81bb72e... 6fgddgdg
-
-            //Destroy(instBullet, 3.0f);
 
 
-
-
-            //    Vector3 dir = target.transform.position - instBulletRigidBody.transform.localPosition;
-
-            //float distThisFrame = bulletSpeed * Time.deltaTime;
-
-            //if (dir.magnitude <= distThisFrame) {
-            //    // We reached the node
-            //    Debug.Log("HIT!");
-            //} else {
-            //    // TODO: Consider ways to smooth this motion.
-
-            //    //Move towards node
-            //    target.transform.Translate(dir.normalized * distThisFrame, Space.World);
-            //    Quaternion targetRotation = Quaternion.LookRotation(dir);
-            //    instBulletRigidBody.transform.rotation = Quaternion.Lerp(instBulletRigidBody.transform.rotation, targetRotation, Time.deltaTime * 5);
-
-
-
-
-
-
+            Destroy(instBullet, 3.0f);
         }
     }
-    }
-<<<<<<< HEAD
 }
-=======
-
->>>>>>> parent of 81bb72e... 6fgddgdg
