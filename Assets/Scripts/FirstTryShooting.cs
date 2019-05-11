ee43d23aca5cd5dd970e10b1d4b0fcbe5ed6bba8
mlindfors2 <31898439+mlindfors2@users.noreply.github.com>
2019-05-11 11:15:31 +0200

    First terrain
    first terrain

--- Assets/Scripts/FirstTryShooting.cs
+++ Assets/Scripts/FirstTryShooting.cs
@@ -7,6 +7,8 @@ public class FirstTryShooting : MonoBehaviour {
     public GameObject bullet;
     public float bulletSpeed = 100f;
     public GameObject target;
+    public GameObject cannon;
+    public GameObject cannonPipe;
 
     // Start is called before the first frame update
     void Start() {
@@ -16,17 +18,21 @@ public class FirstTryShooting : MonoBehaviour {
     // Update is called once per frame
     void Update() {
         if (Input.GetKeyDown(KeyCode.Space)) {
-            GameObject instBullet = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
+            //GameObject instBullet = (GameObject)Instantiate(bullet, transform.position, Quaternion.identity);
+            cannon.transform.LookAt(target.transform.position);
+            GameObject instBullet = (GameObject)Instantiate(bullet, cannonPipe.transform.position, Quaternion.identity);
             Rigidbody instBulletRigidBody = instBullet.GetComponent<Rigidbody>();
 
             //instBulletRigidBody.AddForce(target.transform.position * bulletSpeed);
             //instBulletRigidBody.AddForce(Vector3.forward * bulletSpeed);
+            
+            //instBulletRigidBody.AddForce((target.transform.position - cannonPipe.transform.position).normalized * bulletSpeed);
 
-            instBulletRigidBody.AddForce((target.transform.position - this.transform.position).normalized * bulletSpeed);
+            instBulletRigidBody.AddForce(-(cannonPipe.transform.position- target.transform.position  ).normalized * bulletSpeed); //Bakvänd väg för att testa om de kan bli målsökande
             //instBulletRigidBody.AddForce(Vector3.forward * bulletSpeed);
 
 
-            Destroy(instBullet, 3.0f);
+            Destroy(instBullet, 10.0f);
         }
     }
 }
