35f3e3322af4a7f96aaa91621777311e93aa4536
wklf <wklf@users.noreply.github.com>
2019-05-23 13:50:29 +0200

    score screen ugly!
    asf

---
+++ Assets/Scripts/ReturnToMainMenu.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnToMainMenu : MonoBehaviour
+{
+    public bool IsMainMenu = false;
+
+    public void ButtonMapOne()
+    {
+        //SceneManager.LoadScene("MainMenuScene"); // Or (1) i.e. position in build
+        Debug.Log("Map 1 button method");
+        //GetComponent<Renderer>().material.color = Color.cyan;
+    }
+}
+
+
+
---
+++ Assets/Scripts/ScoreScreenScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScreenScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        //this.gameObject.GetComponent<NavMeshAgent>().speed = movementSpeed;
+
+
+        string t = "";
+        t = "Your score was: \n\n" +
+            "Creeps killed:" + "\n" +
+            "Score: " + "\n" +
+            "Gold gained: " + "\n";
+       
+             this.gameObject.GetComponent<Text>().text = t;
+
+        // var myVariable : float;
+
+        //PlayerPrefs.SetFloat("Player Score", 10.0);
+
+        //myVariable = PlayerPrefs.GetFloat("Player Score");
+
+        //print(myVariable);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
